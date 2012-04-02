a4febaf37f8595ed370ab86d454e660eb3e0e1ec
Jeff Rowberg <jeff@rowberg.net>
2012-04-02 03:08:56 -0400

    Added note about INT pin connection in DMP example sketch.

--- MPU6050/Examples/MPU6050_DMP6/MPU6050_DMP6.ino
+++ MPU6050/Examples/MPU6050_DMP6/MPU6050_DMP6.ino
@@ -45,6 +45,16 @@ THE SOFTWARE.
 // AD0 high = 0x69
 MPU6050 accelgyro;
 
+// =============================================================
+// =============================================================
+//                            NOTE!
+// =============================================================
+// =============================================================
+// In addition to connection 3.3v, GND, SDA, and SCL, this sketch
+// depends on the MPU-6050's INT pin being connected to the
+// Arduino's external interrupt #0 pin. On the Arduino Mega 2560,
+// this is digital I/O pin 2.
+
 uint8_t fifoCount;
 uint8_t fifoBuffer[128];
 uint8_t mpuIntStatus;
