ffed2d239f80cb972c1b80bb5a00091190ec6922
Andreas Zoellner <zoellner@stanford.edu>
2013-03-27 18:38:08 -0700

    Initial Version of I2Cdev and MPU6050 Library for MSP430

---
+++ MSP430/I2Cdev/ArduinoWrapper.h
@@ -0,0 +1,23 @@
+/*
+ * ArduinoWrapper.h
+ *
+ *  Created on: Mar 27, 2013
+ *      Author: zoellner
+ */
+
+#ifndef ARDUINOWRAPPER_H_
+#define ARDUINOWRAPPER_H_
+
+
+
+//Standard Libraries
+#include <stdint.h>
+
+#include "usci_i2c.h"
+
+//functions needed:
+//millis()
+#define millis() 0
+
+
+#endif /* ARDUINOWRAPPER_H_ */
