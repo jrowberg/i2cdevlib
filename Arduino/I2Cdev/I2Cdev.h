3bf675685734ab12fb39914807080c1f118cce99
James Smith <james@loopj.com>
2015-03-29 23:46:55 -0700

    Add 'out of the box' support for Spark Core devices (arduino compatible)

--- Arduino/I2Cdev/I2Cdev.h
+++ Arduino/I2Cdev/I2Cdev.h
@@ -84,6 +84,11 @@ THE SOFTWARE.
     #endif
 #endif
 
+#ifdef SPARK
+    #include <spark_wiring_i2c.h>
+    #define ARDUINO 101
+#endif
+
 // 1000ms default read timeout (modify with "I2Cdev::readTimeout = [ms];")
 #define I2CDEV_DEFAULT_READ_TIMEOUT     1000
 
