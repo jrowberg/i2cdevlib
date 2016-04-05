6744ce0ac434e1a693a31d35d3414f63a7a39fb9
PaulStoffregen <paul@pjrc.com>
2016-04-05 06:31:00 -0700

    Don't do anything special for Teensy, just use regular Arduino Wire lib

--- Arduino/I2Cdev/I2Cdev.h
+++ Arduino/I2Cdev/I2Cdev.h
@@ -79,10 +79,7 @@ THE SOFTWARE.
     #else
         #include "Arduino.h"
     #endif
-    #if defined(CORE_TEENSY) && defined(__MK20DX256__)
-        #include <i2c_t3.h>
-        #define BUFFER_LENGTH 32
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         #include <Wire.h>
     #endif
     #if I2CDEV_IMPLEMENTATION == I2CDEV_I2CMASTER_LIBRARY
