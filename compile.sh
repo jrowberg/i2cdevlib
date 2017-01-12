5396ccf97bada88fe8a51a1c4974550f96372105
tzl0031 <tzl0031@tigermail.auburn.edu>
2017-01-11 18:32:56 -0600

    use double

---
+++ compile.sh
@@ -0,0 +1,2 @@
+g++ -o ADXL345_example_1 /home/pi/i2cdevlib/ADXL345_example_1.cpp \-I/home/pi/i2cdevlib/RaspberryPi_bcm2835/I2Cdev /home/pi/i2cdevlib/RaspberryPi_bcm2835/I2Cdev/I2Cdev.cpp \-I/home/pi/i2cdevlib/Arduino/ADXL345/ /home/pi/i2cdevlib/Arduino/ADXL345/ADXL345.cpp -l bcm2835 -l m
+
