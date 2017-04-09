84d7ed4a3c9f2b9a0a962bde3dcdb16e97cbd487
tzl0031 <tzl0031@tigermail.auburn.edu>
2017-04-09 16:21:36 -0500

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,5 @@
-g++ -o ADXL345_example_1 ADXL345_example_1.cpp \-I/home/pi/i2cdevlib/RaspberryPi_bcm2835/I2Cdev /home/pi/i2cdevlib/RaspberryPi_bcm2835/I2Cdev/I2Cdev.cpp \-I/home/pi/i2cdevlib/Arduino/ADXL345/ 
+g++ -o ADXL345_example_1 ADXL345_example_1.cpp \-I/home/pi/i2cdevlib/RaspberryPi_bcm2835/I2Cdev 
+/home/pi/i2cdevlib/RaspberryPi_bcm2835/I2Cdev/I2Cdev.cpp \-I/home/pi/i2cdevlib/Arduino/ADXL345/ 
 /home/pi/i2cdevlib/Arduino/ADXL345/ADXL345.cpp  
 -std=c++11 -l bcm2835 -l m 
 
