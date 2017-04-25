d8e14de306685aa56b4564d145f227501d6f9af1
tzl0031 <tzl0031@tigermail.auburn.edu>
2017-04-24 21:13:06 -0500

    Update ADXL345_example_1.cpp

--- ADXL345_example_1.cpp
+++ ADXL345_example_1.cpp
@@ -104,8 +104,8 @@ int main(int argc, char **argv) {
     struct sigaction sa;
     struct itimerval value;
     memset(&sa, 0, sizeof(sa));
-    sa.sa_handler = &timer_hander;
-    sigaction(SIGVTALRM, &sa, NULL)
+    sa.sa_handler = &timer_handler;
+    sigaction(SIGVTALRM, &sa, NULL;
     value.it_value.tv_sec=0;                
     value.it_value.tv_usec=300;
     value.it_interval.tv_sec=0;             
