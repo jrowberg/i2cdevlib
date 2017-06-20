a720660616d1b601b189321f036aa80ea38171b9
tzl0031 <tzl0031@tigermail.auburn.edu>
2017-06-20 11:32:07 -0500

    Create Sensor.cpp

---
+++ Sensor.cpp
@@ -0,0 +1,39 @@
+
+#include "Sensor.h"
+//The constructor initialize sensor_id, temperature, clock and date
+
+
+
+Sensor::Sensor(int i):index(i)
+{
+    Json::Reader reader;
+    Json::Value root;
+    int16_t x, y, z;
+    if(i==0)
+    ADXL345 sensor;
+    else
+    ADXL345 sensor(ADXL345_ADDRESS_ALT_HIGH);
+    sensor.getAcceleration(&x, &y, &z);
+    }
+
+
+
+
+
+
+
+
+int Sensor::get_x()
+{
+    return x;
+}
+
+int Sensor::get_y()
+{
+    return y;
+}
+
+int Sensor::get_z()
+{
+    return z;
+}
