ef80e7b7626e637648526367a1482f41a63a77c6
tzl0031 <tzl0031@tigermail.auburn.edu>
2017-06-21 11:07:14 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -108,7 +108,7 @@ int main(int argc, char **argv) {
 	  pthread_t thread1, thread2;
         pthread_create(&thread1, NULL, sensor_1, NULL);
                 pthread_create(&thread2, NULL, sensor_2, NULL);
-	  pthread_join(thread1, NULL);	
+
 		pthread_join(thread2, NULL);			
 	}
 
