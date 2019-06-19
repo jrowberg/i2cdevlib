f6a7ef745e80995ddcaa819a857f7a63cef7d9de
gdsports <gdsports625@gmail.com>
2019-06-18 20:05:07 -0700

    Add RAM cache attribute to ISR
    Required starting with ESP8266 board package 2.5.1
    
    https://github.com/esp8266/Arduino/pull/5995

--- Arduino/MPU6050/examples/MPU6050_DMP6_ESPWiFi/MPU6050_DMP6_ESPWiFi.ino
+++ Arduino/MPU6050/examples/MPU6050_DMP6_ESPWiFi/MPU6050_DMP6_ESPWiFi.ino
@@ -146,7 +146,7 @@ const unsigned int outPort = 9999;          // remote port to receive OSC
 // ================================================================
 
 volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
+void ICACHE_RAM_ATTR dmpDataReady() {
     mpuInterrupt = true;
 }
 
