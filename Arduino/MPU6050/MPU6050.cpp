9554f4e7ac541abceb0723b8bb03f6ec0166cacb
Homer Creutz <zhomeslice@gmail.com>
2022-04-11 09:40:37 -0600

    Lets Store it directly to the variable offsets
    Now that we are not converting to a float we can just store the data directly to the offsets variable and return the pointer

--- Arduino/MPU6050/MPU6050.cpp
+++ Arduino/MPU6050/MPU6050.cpp
@@ -3373,17 +3373,13 @@ void MPU6050_Base::PID(uint8_t ReadAddress, float kP,float kI, uint8_t Loops){
 
 int16_t * MPU6050_Base::GetActiveOffsets() {
     uint8_t AOffsetRegister = (getDeviceID() < 0x38 )? MPU6050_RA_XA_OFFS_H:0x77;
-    int16_t Data[6]{0};
-    if(AOffsetRegister == 0x06)	I2Cdev::readWords(devAddr, AOffsetRegister, 3, (uint16_t *)Data, I2Cdev::readTimeout, wireObj);
+    if(AOffsetRegister == 0x06)	I2Cdev::readWords(devAddr, AOffsetRegister, 3, (uint16_t *)offsets, I2Cdev::readTimeout, wireObj);
     else {
-        I2Cdev::readWords(devAddr, AOffsetRegister, 1, (uint16_t *)Data, I2Cdev::readTimeout, wireObj);
-        I2Cdev::readWords(devAddr, AOffsetRegister+3, 1, (uint16_t *)(Data+1), I2Cdev::readTimeout, wireObj);
-        I2Cdev::readWords(devAddr, AOffsetRegister+6, 1, (uint16_t *)(Data+2), I2Cdev::readTimeout, wireObj);
+        I2Cdev::readWords(devAddr, AOffsetRegister, 1, (uint16_t *)offsets, I2Cdev::readTimeout, wireObj);
+        I2Cdev::readWords(devAddr, AOffsetRegister+3, 1, (uint16_t *)(offsets+1), I2Cdev::readTimeout, wireObj);
+        I2Cdev::readWords(devAddr, AOffsetRegister+6, 1, (uint16_t *)(offsets+2), I2Cdev::readTimeout, wireObj);
     }
-    I2Cdev::readWords(devAddr, 0x13, 3, (uint16_t *)(Data+3), I2Cdev::readTimeout, wireObj);
-    for(uint i = 0; i < 6; i++){
-        offsets[i] = Data[i];
-    }
+    I2Cdev::readWords(devAddr, 0x13, 3, (uint16_t *)(offsets+3), I2Cdev::readTimeout, wireObj);
     return offsets;
 }
 
