6f13aa45bf86d8546481d1c30fbfe9c4f4769100
i164j9 <audiotec69@gmail.com>
2022-04-10 19:48:27 -0500

    Update MPU6050.cpp
    added the ability to return the offsets so they can be passed as arguments, for "self calibration" its a little hacky but it works. its a copy of PrintActiveOffsets() with all of the print statements removed.

--- Arduino/MPU6050/MPU6050.cpp
+++ Arduino/MPU6050/MPU6050.cpp
@@ -3371,6 +3371,29 @@ void MPU6050_Base::PID(uint8_t ReadAddress, float kP,float kI, uint8_t Loops){
 	resetDMP();
 }
 
+float * MPU6050_Base::GetActiveOffsets() {
+    
+	uint8_t AOffsetRegister = (getDeviceID() < 0x38 )? MPU6050_RA_XA_OFFS_H:0x77;
+	int16_t Data[6]{0};
+    int16_t Data1[3]{0};
+	if(AOffsetRegister == 0x06)	I2Cdev::readWords(devAddr, AOffsetRegister, 3, (uint16_t *)Data, I2Cdev::readTimeout, wireObj);
+	else {
+		I2Cdev::readWords(devAddr, AOffsetRegister, 1, (uint16_t *)Data, I2Cdev::readTimeout, wireObj);
+		I2Cdev::readWords(devAddr, AOffsetRegister+3, 1, (uint16_t *)Data+1, I2Cdev::readTimeout, wireObj);
+		I2Cdev::readWords(devAddr, AOffsetRegister+6, 1, (uint16_t *)Data+2, I2Cdev::readTimeout, wireObj);
+	}
+    delay(500);
+	I2Cdev::readWords(devAddr, 0x13, 3, (uint16_t *)Data1, I2Cdev::readTimeout, wireObj);
+    delay(500);
+    Data[3] = Data1[0];
+    Data[4] = Data1[1];
+    Data[5] = Data1[2];
+    for(uint i = 0; i < 6; i++){
+        offsets[i] = Data[i];
+    }
+    return offsets;
+}
+
 void MPU6050_Base::PrintActiveOffsets() {
 	uint8_t AOffsetRegister = (getDeviceID() < 0x38 )? MPU6050_RA_XA_OFFS_H:0x77;
 	int16_t Data[3];
