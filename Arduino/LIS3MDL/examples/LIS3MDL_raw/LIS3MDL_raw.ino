dca7467d24fc307449230e3dbd3835a8adb76f3f
Braidan <legohead259@gmail.com>
2022-12-04 22:20:10 -0500

    Added LIS3MDL arduino example

--- Arduino/LIS3MDL/examples/LIS3MDL_raw/LIS3MDL_raw.ino
+++ Arduino/LIS3MDL/examples/LIS3MDL_raw/LIS3MDL_raw.ino
@@ -36,53 +36,56 @@ THE SOFTWARE.
 // I2Cdev and L3G4200D must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
 #include <I2Cdev.h>
-#include <L3G4200D.h>
+#include <LIS3MDL.h>
 
 // default address is 105
 // specific I2C address may be passed here
-L3G4200D gyro;
+LIS3MDL mag;
 
-int16_t avx, avy, avz;
+int16_t magx, magy, magz;
 
-#define LED_PIN 13 // (Arduino is 13, Teensy is 6)
 bool blinkState = false;
 
 void setup() {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    Wire.begin();
+    Wire.begin(SDA, SCL);
 
-    // initialize serial communication
-    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
-    // it's really up to you depending on your project)
     Serial.begin(9600);
 
     // initialize device
-    Serial.println("Initializing I2C devices...");
-    gyro.initialize();
+    Serial.print("Initializing I2C devices...");
+    if (!mag.initialize()) {
+        Serial.println("Failed to find LIS3MDL!");
+    }
+    Serial.println("done!");
 
     // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(gyro.testConnection() ? "L3G4200D connection successful" : "L3G4200D connection failed");
+    Serial.print("Testing device connections...");
+    Serial.println(mag.testConnection() ? "L3G4200D connection successful" : "L3G4200D connection failed");
 
     // configure LED for output
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-    // data seems to be best when full scale is 2000
-    gyro.setFullScale(2000);
+    // Configure device
+    mag.setXYOpMode(LIS3MDL_OM_HP_MODE); // Set XY-axes to High Performance mode
+    mag.setZOpMode(LIS3MDL_OMZ_HP_MODE); // Set Z-axis to High Performance mode
+    mag.setDataRate(LIS3MDL_DATA_RATE_80_HZ); // Set sample rate to 80 Hz
+    mag.setFullScale(LIS3MDL_FULL_SCALE_4_G); // Set scale to ±4 Gauss
 }
 
 void loop() {
     // read raw angular velocity measurements from device
-    gyro.getAngularVelocity(&avx, &avy, &avz);
+    mag.getMagneticField(&magx, &magy, &magz);
 
-    Serial.print("angular velocity:\t");
-    Serial.print(avx); Serial.print("\t");
-    Serial.print(avy); Serial.print("\t");
-    Serial.println(avz);
+    Serial.print("Magnetic field \t");
+    Serial.print("X: "); Serial.print(magx); Serial.print("\t");
+    Serial.print("Y: "); Serial.print(magy); Serial.print("\t");
+    Serial.print("Z: "); Serial.println(magz);
 
     // blink LED to indicate activity
     blinkState = !blinkState;
     digitalWrite(LED_PIN, blinkState);
+
+    delay(1000/80); // Try to run every 1/80 of a second
 }
 
 
