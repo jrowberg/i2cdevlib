4ee3e3c12ed864c4838728b7574397c1de3961fe
Hasan Fırat Keskin <52783312+spacemonochrome@users.noreply.github.com>
2025-04-28 00:18:24 +0300

    Update I2Cdev.h
    For Build Error

--- STM32HAL/I2Cdev/I2Cdev.h
+++ STM32HAL/I2Cdev/I2Cdev.h
@@ -47,7 +47,7 @@ typedef int bool;
 #define true 1
 #define false 0
 
-uint16_t I2Cdev_readTimeout;
+extern uint16_t I2Cdev_readTimeout;
 
 // 1000ms default read timeout (modify with "I2Cdev::readTimeout = [ms];")
 #define I2CDEV_DEFAULT_READ_TIMEOUT     1000
